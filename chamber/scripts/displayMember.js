1f0f4b1888796a9560ae9935b5965a7f9e023e7e
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2024-11-15 09:02:13 -1000

    added images to the buttons for grid and list view

--- chamber/scripts/displayMember.js
+++ chamber/scripts/displayMember.js
@@ -1,22 +1,55 @@
-const membersDiv = document.querySelector("#members");
+const membersDiv = document.querySelector('#members');
+const grid = document.querySelector('#gridButton');
+const list = document.querySelector('#listButton');
 
 async function fetchMemberData() {
-  const response = await fetch("data/members.json");
-  const members = await response.json();
-  // console.log(members);
-  members.forEach((member) => {
-    const memberDiv = document.createElement("div");
-    memberDiv.classList.add("member-card");
-    memberDiv.innerHTML = `
-			<h3>${member.name}</h3>
-			<p>${member.address}</p>
-			<p>${member.phone}</p>
-			<a href="${member.website}" target="_blank">${member.website}</a>
-			<figure>
-			<img src="images/${member.image}" alt="${member.name}"width="200" height="auto"Logo>
-			</figure>
-		`;
-    membersDiv.appendChild(memberDiv);
+  try {
+    const response = await fetch('data/members.json');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const members = await response.json();
+
+    members.forEach((member) => {
+      const memberDiv = document.createElement('div');
+      memberDiv.classList.add('member-card');
+      memberDiv.innerHTML = `
+				<h3>${member.name}</h3>
+				<p>${member.address}</p>
+				<p>${member.phone}</p>
+				<a href="${member.website}" target="_blank">${member.website}</a>
+				<figure>
+				<img src="images/${member.image}" alt="${member.name}"width="200" height="auto">
+				</figure>
+			`;
+      membersDiv.appendChild(memberDiv);
+    });
+
+    membersDiv.classList.add('grid');
+    membersDiv.classList.remove('list');
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+function setActiveButton(button) {
+  gridButton.classList.remove('active');
+  listButton.classList.remove('active');
+  button.classList.add('active');
+}
+
+if (gridButton) {
+  grid.addEventListener('click', () => {
+    membersDiv.classList.add('grid');
+    membersDiv.classList.remove('list');
+    setActiveButton(gridButton);
+  });
+}
+if (listButton) {
+  list.addEventListener('click', () => {
+    membersDiv.classList.add('list');
+    membersDiv.classList.remove('grid');
+    setActiveButton(listButton);
   });
 }
 
--- chamber/scripts/views.js
+++ chamber/scripts/views.js
@@ -1,1 +1,6 @@
-// grab elements from the dom
+// create a grid view and a list view of the members cards
+
+const grid = document.querySelector('.gridButton');
+const list = document.querySelector('.listButton');
+
+// create a fucntion with add eventlistener
